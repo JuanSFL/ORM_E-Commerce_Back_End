1e32f670b08c0b0b715824d232b421ff63ad5fc1
JuanSFL <molina98j@yahoo.com>
2023-01-07 21:12:11 -0500

    Added routes and established relationships between tables

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,8 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
+      // if not 8889, change to the port that your SQL uses.
+      port: 8889,
       dialectOptions: {
         decimalNumbers: true,
       },
